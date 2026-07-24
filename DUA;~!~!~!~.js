c45edaef916bafa22c38694926d7a90a0fca123f
Terran Adams <60047420+terranadams@users.noreply.github.com>
2026-07-24 09:14:23 -0600

    Delete DUA;~!~!~!~.js

--- DUA;~!~!~!~.js
+++
@@ -1,7 +0,0 @@
-if (matches(currentUserID, "ADMIN", "KRICE", "WSTENERSON", "BYRNE")) { showMessage = true; showDebug = true; }
-
-logDebug('|==========================================DUA Event started =================================================|');
-
-aa.print('|==========================================DUA Event started =================================================|');
-
- 
